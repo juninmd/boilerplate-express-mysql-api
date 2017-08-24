433e481f9849635da1aa4404e52b2fa206d8cb7b
juninmd <jr_acn@yahoo.com.br>
2017-08-23 21:35:00 -0300

    Novos métodos

--- bin/controller/errorCtl.js
+++ bin/controller/errorCtl.js
@@ -1,4 +1,15 @@
 module.exports = (app) => {
+
+    app.get("/error/function", (req, res, next) => {
+        setTimeout(function() {
+            kkkkk
+        }, 2000);
+    })
+
+    app.get("/error/next", (req, res, next) => {
+        next(new Error('aaa'));
+    })
+
     app.get("/error", (req, res, next) => {
         for (i = 0; i < 999; i++) {
             console.info('aaaaa');
@@ -6,4 +17,5 @@ module.exports = (app) => {
         batata
 
     })
+
 }
